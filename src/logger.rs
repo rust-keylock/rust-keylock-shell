b86ecc5861f8ce05f60ad2c726d5808ba2c13525
aston <astonbitecode@gmail.com>
2017-09-26 10:31:16 +0300

    Upgraded some of the libs

--- src/logger.rs
+++ src/logger.rs
@@ -1,14 +1,50 @@
 use log;
 use fern;
 use chrono;
+use std::fmt;
+use std::error::Error;
+use std::io;
 
-pub fn init_logging() {
-    let logger_config = fern::DispatchConfig {
-        format: Box::new(move |msg: &str, level: &log::LogLevel, location: &log::LogLocation| {
-            format!("[{}][{}][{}] {}", chrono::Local::now().format("%Y-%m-%d][%H:%M:%S.%f"), level, location.module_path(), msg)
-        }),
-        output: vec![fern::OutputConfig::file("rust-keylock.log")],
-        level: log::LogLevelFilter::Debug,
-    };
-    let _ = fern::init_global_logger(logger_config, log::LogLevelFilter::Debug);
+pub fn init_logging() -> Result<(), ShellLoggerError> {
+    fern::Dispatch::new().format(|out, message, record| {
+            out.finish(format_args!("{}[{}][{}] {}",
+                                    chrono::Local::now().format("[%Y-%m-%d][%H:%M:%S]"),
+                                    record.target(),
+                                    record.level(),
+                                    message))
+        })
+        .level(log::LogLevelFilter::Debug)
+        .chain(fern::log_file("output.log")?)
+        .apply()?;
+
+	Ok(())
+}
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub struct ShellLoggerError {
+    error_message: String
+}
+
+impl fmt::Display for ShellLoggerError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    	write!(f, "{}", self.error_message)
+	}
+}
+
+impl Error for ShellLoggerError {
+    fn description(&self) -> &str {
+    	"ShellLoggerError"
+    }
+}
+
+impl From<io::Error> for ShellLoggerError {
+    fn from(err: io::Error) -> ShellLoggerError {
+    	ShellLoggerError {error_message: format!("{:?}", err)}
+    }
+}
+
+impl From<log::SetLoggerError> for ShellLoggerError {
+    fn from(err: log::SetLoggerError) -> ShellLoggerError {
+    	ShellLoggerError {error_message: format!("{:?}", err)}
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,10 @@ mod shell;
 
 #[allow(dead_code)]
 fn main() {
-    logger::init_logging();
+    let res = logger::init_logging();
+    if res.is_err() {
+    	println!("Could not initialize logger! Reason: {}", res.err().unwrap())
+    }
     let shell = shell::new();
     rust_keylock::execute(&shell);
 }
